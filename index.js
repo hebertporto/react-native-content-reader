96005a648636a41d215657bd54a16020b72953f4
Hebert Porto <hebertporto@gmail.com>
2018-05-02 23:33:27 -0300

    add linter validation, init structure setup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from 'react-native'
+import { SuperNovelReader } from './src'
 
-AppRegistry.registerComponent('superleitorapp', () => App);
+AppRegistry.registerComponent('superleitorapp', () => SuperNovelReader)
--- src/app.js
+++ src/app.js
@@ -1,33 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View
-} from 'react-native';
-import SplashScreen from 'react-native-splash-screen'
+} from 'react-native'
 import { RkButton } from 'react-native-ui-kitten'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-export default class App extends Component {
-  componentDidMount() {
-    // do stuff while splash screen is shown
-      // After having done stuff (such as async tasks) hide the splash screen
-      SplashScreen.hide();
-  }
-
+export class App extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -42,7 +29,7 @@ export default class App extends Component {
         </Text>
         <RkButton>Click me!</RkButton>
       </View>
-    );
+    )
   }
 }
 
@@ -51,16 +38,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
