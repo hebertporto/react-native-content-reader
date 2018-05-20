023c4005d34cf5f57cdc9e652528d0f7b5dee0b5
Hebert Porto <hebertporto@gmail.com>
2018-05-19 23:09:14 -0300

    styling novel screen

--- src/modules/novel/components/Novel.js
+++ src/modules/novel/components/Novel.js
@@ -14,18 +14,40 @@ const Novel = () => {
     <ImageBackground
       source={{
         uri: 'http://res.cloudinary.com/dwvrdf3zg/image/upload/v1510765331/gnoadvlyv9t9zv4h2ery.png' }}
-      style={styles.container}
+      style={styles.containerImageBackground}
     >
       <ScrollView>
-        <View style={styles.box}>
-          <View style={styles.imageContainer}>
+        <View style={styles.imageCardContainer}>
+          <View style={styles.imageBox}>
             <Image
               style={styles.image}
               source={{
                 uri: 'http://res.cloudinary.com/dwvrdf3zg/image/upload/v1510765331/gnoadvlyv9t9zv4h2ery.png' }}
             />
           </View>
-          <Text>opa</Text>
+        </View>
+        <View style={styles.bodyContainer}>
+          <View style={styles.bodyContent}>
+            <Text style={styles.title}>
+              Rebirth of the Thief Who Roamed the World
+            </Text>
+          </View>
+          <View>
+            <Text>Autor, Scan, Tradutor, Revisor</Text>
+          </View>
+          <View>
+            <Text>Capítulos</Text>
+          </View>
+          <View>
+            <Text>
+              Descrição
+            </Text>
+          </View>
+          <View>
+            <Text>
+              Lista de Capítulos
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </ImageBackground>
--- src/modules/novel/components/styles/Novel.style.js
+++ src/modules/novel/components/styles/Novel.style.js
@@ -1,42 +1,54 @@
 import { StyleSheet, Dimensions } from 'react-native'
+import { Fonts } from '../../../../constants/fonts'
 
 const { width } = Dimensions.get('window')
 
-const boxWidth = width - 80
+const boxWidth = width - 70
+const bottonBodyContainer = 80
+const textPadding = bottonBodyContainer + 20
+
 export const styles = StyleSheet.create({
-  container: {
+  containerImageBackground: {
     height: '100%',
     width: '100%',
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center'
   },
-  box: {
-    width: boxWidth,
-    height: 1000,
-    borderRadius: 10,
-    borderWidth: 0,
-    backgroundColor: '#fff',
-    marginTop: 180
+  imageCardContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 35,
+    elevation: 15
   },
-  imageContainer: {
-    height: 200,
-    width: 150,
+  imageBox: {
+    height: 250,
+    width: boxWidth - 70,
     borderRadius: 10,
-    borderWidth: 0,
-    borderColor: 'transparent',
-    borderBottomWidth: 0,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.8,
-    shadowRadius: 10,
-    elevation: 5,
-    marginLeft: 5,
-    marginRight: 5,
-    marginTop: -50
+    borderWidth: 0
   },
   image: {
     borderRadius: 10,
     ...StyleSheet.absoluteFillObject
+  },
+  bodyContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: boxWidth,
+    borderRadius: 10,
+    borderWidth: 0,
+    backgroundColor: '#fff',
+    bottom: bottonBodyContainer
+  },
+  bodyContent: {
+    paddingTop: textPadding,
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    ...Fonts.style.h6,
+    color: '#000',
+    textAlign: 'center'
   }
 })
